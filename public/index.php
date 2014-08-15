f5cf9b5b14e61675dd10392421763c532a940f89
V. Rositskyy <v.rositskyy@doc.ua>
2014-08-15 09:53:05 +0300

    index file

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vovanada
+ * Date: 14.08.14
+ * Time: 19:48
+ */
+$webRoot = dirname(__FILE__);
+define('PROJECT_ROOT', $webRoot . '/..');
+require_once(PROJECT_ROOT . '/vendor/autoload.php');
+
